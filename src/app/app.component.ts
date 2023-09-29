ece137c9b572553adf079a00491345e17f084ee1
biruk-ay <137400651+biruk-ay@users.noreply.github.com>
2023-09-29 15:15:02 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent {
   
   async getData() {
     try {
-      const response = await axios.get("https://01b72cc91a3d33.lhr.life");
+      const response = await axios.get("http://172.18.11.43/");
       console.log(response.data)
       this.result = response.data;
     }
